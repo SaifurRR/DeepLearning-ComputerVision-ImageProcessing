fe6d318e2ba0409018ca52f8163dfd51d14457c2
SaifurR <90628681+SaifurRR@users.noreply.github.com>
2023-10-26 23:41:07 -0700

    Add files via upload
    open the image and wait for a key-stroke to close the image.

---
+++ Image Processing/open_img_puppy.py
@@ -0,0 +1,16 @@
+import cv2
+
+img=cv2.imread('puppy_image.jpg')
+
+#img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+while True:
+    cv2.imshow('puppy', img)
+    # waited 1msec AND we've pressed 'q' key 
+    if cv2.waitKey(1) & 0xFF ==ord('q'):     
+        break;
+        
+cv2.destroyAllWindows() 
+
+
+
