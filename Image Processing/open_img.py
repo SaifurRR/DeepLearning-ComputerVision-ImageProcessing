6679bda46ee387966cc9790f753fcae3edc3572a
SaifurR <90628681+SaifurRR@users.noreply.github.com>
2023-10-31 10:25:07 -0700

    Update open_img.py

--- Image Processing/open_img.py
+++ Image Processing/open_img.py
@@ -7,7 +7,7 @@ img=cv2.imread('puppy_image.jpg')
 while True:
     cv2.imshow('puppy', img)
     # waited 1msec AND we've pressed 'q' key 
-    if cv2.waitKey(1) & 0xFF ==ord('q'):     
+    if cv2.waitKey(1) & (0xFF ==ord('q')):     
         break;
         
 cv2.destroyAllWindows() 
